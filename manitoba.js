1b9b9b8ee9f6dd59643d4aad30781b700298d5d0
Matt Hadden <forceh2010@gmail.com>
2022-06-14 18:24:15 -0400

    feat(manitoba): add yesterday precip data

--- manitoba.js
+++ manitoba.js
@@ -2,17 +2,24 @@ const axios = require("axios");
 const Weather = require("ec-weather-js");
 
 const manitobaCities = [
+  { name: "Winnipeg", stationCode: "MB/s0000193" },
+  { name: "Portage", stationCode: "MB/s0000626" },
   { name: "Brandon", stationCode: "MB/s0000492" },
   { name: "Dauphin", stationCode: "MB/s0000508" },
   { name: "Kenora", stationCode: "ON/s0000651" },
-  { name: "Portage", stationCode: "MB/s0000626" },
   { name: "Thompson", stationCode: "MB/s0000695" },
-  { name: "Winnipeg", stationCode: "MB/s0000193" },
 ];
 
 const highLowAroundMB = [];
 function fetchHighLowAroundMB() {
-  const results = [...manitobaCities.map((station) => ({ ...station, temp: null, temp_class: null }))];
+  const results = [
+    ...manitobaCities.map((station) => ({
+      ...station,
+      temp: null,
+      temp_class: null,
+      precip: {},
+    })),
+  ];
 
   const promises = [];
   manitobaCities.forEach((station) => {
@@ -24,16 +31,28 @@ function fetchHighLowAroundMB() {
           const weather = new Weather(data);
           if (!weather) throw "Unable to parse weather data";
 
-          const immediateForecast = weather.weekly && weather.weekly[0];
-          if (!immediateForecast) throw "No weekly forecast available";
-
           const resultIx = results.findIndex((s) => s.name === station.name);
-          if (resultIx !== -1)
+          if (resultIx === -1) return;
+
+          const immediateForecast = weather.weekly && weather.weekly[0];
+          if (immediateForecast) {
             results[resultIx] = {
               ...results[resultIx],
               temp: immediateForecast.temperatures?.temperature?.value,
               temp_class: immediateForecast.temperatures?.temperature?.class,
             };
+          }
+
+          const yesterdayConditions = weather.all.yesterdayConditions;
+          if (!yesterdayConditions) return;
+
+          const yesterdayPrecip = yesterdayConditions.precip;
+          if (!yesterdayPrecip) return;
+
+          results[resultIx] = {
+            ...results[resultIx],
+            precip: yesterdayPrecip || {},
+          };
         })
         .catch(() => {
           console.warn("[MANITOBA]", station.name, "failed to fetch data");

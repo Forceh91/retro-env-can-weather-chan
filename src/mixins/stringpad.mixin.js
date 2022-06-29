e3223bdad88dd551f02a0977ce5411ab7e85b2e0
Matt Hadden <forceh2010@gmail.com>
2022-06-29 17:03:41 -0400

    feat(stringpad): create mixin for string pad

---
+++ src/mixins/stringpad.mixin.js
@@ -0,0 +1,15 @@
+export default {
+  name: "string-pad-mixin",
+  methods: {
+    padString(val, minLength, isFront, char) {
+      if (val === undefined || val === null) return "";
+
+      char = char || `&nbsp;`;
+      const paddingToAdd = minLength - val.length;
+      let paddingString = ``;
+      for (let i = 0; i < paddingToAdd; i++) paddingString += char;
+
+      return !isFront ? `${val}${paddingString}` : `${paddingString}${val}`;
+    },
+  },
+};

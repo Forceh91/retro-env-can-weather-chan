08c71fdb3c62970a67098e7da155b2f8158d3cd6
Matt Hadden <forceh2010@gmail.com>
2022-07-03 15:35:20 -0400

    refactor(citystats): improve h/c spot string

--- src/components/citystats.vue
+++ src/components/citystats.vue
@@ -15,11 +15,19 @@
 </template>
 
 <script>
-const HOT_COLD_SPOT_MAX_LENGTH = 24;
 const PRECIP_STRING_WITH_DATA_LENGTH = 29;
+
+// max length the hot/cold spot info can be including dots and temp
+const HOT_COLD_SPOT_CITY_PROV_PAD_TEMP_LENGTH = 30;
+
+// max length for the province with comma, at least two dots, and temp
+const HOT_COLD_SPOT_PROV_PAD_TEMP_LENGTH = 8;
+
+// taking away room for the `, PV .. TMP` gives us how long the city name can be
+const HOT_COLD_SPOT_CITY_MAX_LENGTH = HOT_COLD_SPOT_CITY_PROV_PAD_TEMP_LENGTH - HOT_COLD_SPOT_PROV_PAD_TEMP_LENGTH;
+
 import { format } from "date-fns";
 import stringpadmixin from "../mixins/stringpad.mixin";
-
 // import { EventBus } from "../js/EventBus";
 
 export default {
@@ -74,22 +82,50 @@ export default {
       return `Canadian Hot/Cold Spot - ${this.currentDate}`;
     },
 
+    hotSpotCity() {
+      return (this.hotcold?.hot?.city || "N/A").slice(0, HOT_COLD_SPOT_CITY_MAX_LENGTH);
+    },
+
+    hotSpotProvince() {
+      return this.hotcold?.hot?.province || "N/A";
+    },
+
+    hotSpotTemp() {
+      const temp = this.hotcold?.hot?.temp;
+      const tempString = !isNaN(temp) ? Math.round(temp) : "N/A";
+      return this.padString(`${tempString}`, 3, true);
+    },
+
+    hotSpotEllipsis() {
+      const usedSpace = this.hotSpotCity.length + HOT_COLD_SPOT_PROV_PAD_TEMP_LENGTH;
+      return this.padString("..", HOT_COLD_SPOT_CITY_PROV_PAD_TEMP_LENGTH - usedSpace, false, ".");
+    },
+
     hotSpotString() {
-      return `&nbsp;${(this.hotcold?.hot?.city || "N/A").slice(0, 17)}, ${this.hotcold?.hot?.province ||
-        "N/A"}&nbsp;${this.fillEllipsis(this.hotcold?.hot)}${this.padString(
-        (this.hotcold.hot?.temp || "N/A").split(".")[0],
-        3,
-        true
-      )}`;
+      return `&nbsp;${this.hotSpotCity},&nbsp;${this.hotSpotProvince}&nbsp;${this.hotSpotEllipsis}${this.hotSpotTemp}`;
+    },
+
+    coldSpotCity() {
+      return (this.hotcold?.cold?.city || "N/A").slice(0, HOT_COLD_SPOT_CITY_MAX_LENGTH);
+    },
+
+    coldSpotProvince() {
+      return this.hotcold?.cold?.province || "N/A";
+    },
+
+    coldSpotTemp() {
+      const temp = this.hotcold?.cold?.temp;
+      const tempString = !isNaN(temp) ? Math.round(temp) : "N/A";
+      return this.padString(`${tempString}`, 3, true);
+    },
+
+    coldSpotEllipsis() {
+      const usedSpace = this.coldSpotCity.length + HOT_COLD_SPOT_PROV_PAD_TEMP_LENGTH;
+      return this.padString("..", HOT_COLD_SPOT_CITY_PROV_PAD_TEMP_LENGTH - usedSpace, false, ".");
     },
 
     coldSpotString() {
-      return `&nbsp;${(this.hotcold?.cold?.city || "N/A").slice(0, 17)}, ${this.hotcold?.cold?.province ||
-        "N/A"}&nbsp;${this.fillEllipsis(this.hotcold?.cold)}${this.padString(
-        (this.hotcold.cold?.temp || "N/A").split(".")[0],
-        3,
-        true
-      )}`;
+      return `&nbsp;${this.coldSpotCity},&nbsp;${this.coldSpotProvince}&nbsp;${this.coldSpotEllipsis}${this.coldSpotTemp}`;
     },
 
     precipTitle() {
@@ -97,7 +133,7 @@ export default {
     },
 
     precipActual() {
-      const totalPrecip = `${this.seasonPrecip?.totalPrecip || 0} MM`;
+      const totalPrecip = `${this.padString(this.seasonPrecip?.totalPrecip || 0, 5, true)} MM`;
       const dateString = `${this.isWinter ? `October` : `April`} 1st`;
 
       // how many dots we need here
@@ -108,7 +144,7 @@ export default {
     },
 
     precipNormal() {
-      const normalPrecip = `${this.seasonPrecip?.normalPrecip || 0} MM`;
+      const normalPrecip = `${this.padString(this.seasonPrecip?.normalPrecip || 0, 5, true)} MM`;
       const dateString = `Normal`;
 
       // how many dots we need here
@@ -122,15 +158,6 @@ export default {
   methods: {
     pad(val) {
       return val < 10 ? `0${val}` : val;
-    },
-
-    fillEllipsis(data) {
-      if (!data) return this.padString("", HOT_COLD_SPOT_MAX_LENGTH, true, ".");
-
-      const nameLength = data?.city?.length || 0;
-      const provinceLength = data?.province?.length || 0;
-
-      return this.padString("", HOT_COLD_SPOT_MAX_LENGTH - nameLength - provinceLength, true, ".");
     },
   },
 };

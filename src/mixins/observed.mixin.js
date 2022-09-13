7304a7ec689ff4ab19efba9a49c5e6b1dc434250
Matt Hadden <forceh2010@gmail.com>
2022-09-13 18:21:23 -0400

    refactor(observedmixin): handle spacing better

--- src/mixins/observed.mixin.js
+++ src/mixins/observed.mixin.js
@@ -1,4 +1,4 @@
-import { parseISO, format, addDays } from "date-fns";
+import { parseISO, format, addDays, subDays } from "date-fns";
 
 export default {
   name: "observed-mixin",
@@ -9,10 +9,18 @@ export default {
       const parsedDate = parseISO(date.time);
       const timeZone = date.timezone;
 
-      let hourTimezoneFormat = `hh aa '${timeZone}&nbsp;'`;
+      const hours = parsedDate.getHours();
+      let hourTimezoneFormat = `hh aa `;
+      // we need to double space after timezone if its a single digit hour
+      // otherwise it should be a single space because 10/11 eat into that space
+      let timeZoneString = `'${timeZone}&nbsp;'`;
+      if (hours < 10 || (hours > 12 && hours < 22)) timeZoneString = `'${timeZone}&nbsp;&nbsp;'`;
+
+      // ammend this onto the hour format
+      hourTimezoneFormat += timeZoneString;
+
       // handle midnight/noon
       if (handleMid) {
-        const hours = parsedDate.getHours();
         if (hours === 12) hourTimezoneFormat = `'${this.padString("Noon", 10)}'`;
         if (hours === 0) hourTimezoneFormat = `'${this.padString("Midnight", 10)}'`;
       }
@@ -22,7 +30,7 @@ export default {
       const dateString = format(parsedDate, `${hourTimezoneFormat}${monthDateYearFormat}`);
 
       // clear out leading 0s from time/date as we dont want those
-      const displayDateString = dateString.replace(/0([0-9])/, "&nbsp$1").replace(/\s0/, "&nbsp;&nbsp;");
+      const displayDateString = dateString.replace(/0([0-9])/, "$1").replace(/\s0/, "&nbsp;&nbsp;");
       return displayDateString;
     },
 
@@ -35,6 +43,17 @@ export default {
       const daysAhead = addDays(parsedDate, numberOfDays);
 
       return daysAhead;
+    },
+
+    getDaysBehindFromObserved(date, numberOfDays) {
+      // we can go back anytime but default to 1
+      numberOfDays = numberOfDays || 1;
+
+      // turn it into a date and count back
+      const parsedDate = parseISO(date.time);
+      const daysBehind = subDays(parsedDate, numberOfDays);
+
+      return daysBehind;
     },
   },
 };

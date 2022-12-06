78f8de7d51529dd8a10f17ed2a80d251316d26e3
Matt Hadden <forceh2010@gmail.com>
2022-12-05 19:02:33 -0500

    feat: create an info screen object

---
+++ config/info-screens.js
@@ -0,0 +1,40 @@
+const { isValid, parseISO, compareAsc, startOfToday } = require("date-fns");
+const fs = require("fs");
+
+// 8 lines at 32 chars max each
+const MAX_MESSAGE_LENGTH = 256;
+
+const createInfoScreen = (message, start, end, isInfinite) => {
+  // make sure the message exists and is short enough
+  if (!message || message.length > MAX_MESSAGE_LENGTH) return;
+
+  // make sure we have an end if its not infinite
+  if (!isInfinite && !end) return;
+
+  // make sure start/end is valid
+  if (!start || !isValid(parseISO(start))) return;
+  if (end && !isValid(parseISO(end))) return;
+
+  // make sure start and end are in the future
+  const now = startOfToday();
+  const isStartInFuture = compareAsc(now, parseISO(start));
+  if (isStartInFuture === 1) return;
+
+  const isEndInFuture = compareAsc(now, parseISO(end + "T23:59:59.0Z"));
+  if (isEndInFuture === 1) return;
+
+  // create the object to store
+  const infoScreen = {
+    message,
+    start,
+    end,
+    isInfinite,
+  };
+
+  // save it
+
+  // return it
+  return infoScreen;
+};
+
+module.exports = { createInfoScreen };

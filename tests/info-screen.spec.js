dbe5087e5fad6426f33def6b8be29076387165a4
Matt Hadden <forceh2010@gmail.com>
2022-12-05 22:14:16 -0500

    test: load info screens from config file

--- tests/info-screen.spec.js
+++ tests/info-screen.spec.js
@@ -1,5 +1,6 @@
 import { addDays, format, subDays } from "date-fns";
-import * as configInfoScreens from "../config/info-screens";
+import configInfoScreens from "../config/info-screens";
+import fs from "fs";
 
 const longMessage =
   "a really long info screen\na really long info screen\na really long info screen\na really long info screen\na really long info screen\na really long info screen\na really long info screen\na really long info screen\na really long info screen\na really long info screen\na really long info screen\na really long info screen\n";
@@ -9,8 +10,22 @@ const formattedToday = format(today, "yyyy-MM-dd");
 const formattedYesterday = format(subDays(today, 1), "yyyy-MM-dd");
 const formattedTomorrow = format(addDays(today, 1), "yyyy-MM-dd");
 
+const fakeInfoScreens = {
+  info_screens: [
+    { id: 1, message: "a", start: formattedToday, end: formattedTomorrow },
+    {
+      id: 2,
+      message: "b",
+      start: format(subDays(new Date(), 3), "yyyy-MM-dd"),
+      end: formattedYesterday,
+    },
+    { id: 3, message: "c", start: formattedToday, end: "", isInfinite: true },
+  ],
+};
+
 describe("info-screens.js", () => {
   beforeEach(() => configInfoScreens.getInfoScreens().splice(0));
+  afterEach(() => jest.resetAllMocks());
 
   test("createInfoScreen: checks message length is valid", (done) => {
     expect(configInfoScreens.createInfoScreen()).toBeFalsy();
@@ -84,17 +99,30 @@ describe("info-screens.js", () => {
   });
 
   test("cleanupStaleInfoScreens: cleans up screens correctly", (done) => {
-    configInfoScreens.getInfoScreens().push({ id: 1, message: "a", start: formattedToday, end: formattedTomorrow });
-    configInfoScreens.getInfoScreens().push({
-      id: 2,
-      message: "b",
-      start: format(subDays(new Date(), 3), "yyyy-MM-dd"),
-      end: formattedYesterday,
-    });
-    configInfoScreens.getInfoScreens().push({ id: 2, message: "c", start: formattedToday, end: "", isFinite: true });
+    configInfoScreens.getInfoScreens().push(...fakeInfoScreens.info_screens);
 
     configInfoScreens.cleanupStaleInfoScreens();
     expect(configInfoScreens.getInfoScreens().length).toBe(2);
+    done();
+  });
+
+  test.skip("initInfoScreens: sets up the interval correctly", (done) => {
+    jest.useFakeTimers();
+    const spy = jest.spyOn(configInfoScreens, "cleanupStaleInfoScreens");
+    const timer = configInfoScreens.initInfoScreens();
+    jest.advanceTimersToNextTimer();
+    expect(spy).toHaveBeenCalled();
+    clearInterval(timer);
+    done();
+  });
+
+  test("loadInfoScreens: loads screens correctly", (done) => {
+    const spy = jest
+      .spyOn(fs, "readFile")
+      .mockImplementation((path, options, callback) => callback(null, Buffer.from(JSON.stringify(fakeInfoScreens))));
+    configInfoScreens.loadInfoScreens();
+    expect(spy).toHaveBeenCalled();
+    expect(configInfoScreens.getInfoScreens().length);
     done();
   });
 });

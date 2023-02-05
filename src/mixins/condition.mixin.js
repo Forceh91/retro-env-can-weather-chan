d8d6b8a198fb3094bf5578528fc7f615459cfc9f
Matt Hadden <Forceh91@users.noreply.github.com>
2023-02-05 18:40:23 -0500

    update(sunspot): more forecast conditions (#404)

--- src/mixins/condition.mixin.js
+++ src/mixins/condition.mixin.js
@@ -57,8 +57,14 @@ export default {
       // isolated rain showers (then...)
       condition = condition.replace(/isolated rain showers/gi, "isld showers");
 
-      // slight chance showers
-      condition = condition.replace(/(slight\s)?chance(\srain)? showers/gi, "chnc showers");
+      // slight chance (rain) showers
+      condition = condition.replace(/(slight\s)?chance(\srain)? showers?/gi, "chnc showers");
+
+      // slight chance rain (because thats different to showers, lol)
+      condition = condition.replace(/(slight\s)?chance rain/gi, "chnc rain");
+
+      // slight chance light/heavy rain
+      condition = condition.replace(/(slight\s)?chance (light|heavy) rain/gi, "chc $2 rain");
 
       // scattered (rain) showers
       condition = condition.replace(/scattered(\srain)?\sshowers/gi, "sctd showers");

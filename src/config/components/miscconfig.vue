9e69b381d06a0f3585c0af562026751d888456d7
Matt Hadden <forceh2010@gmail.com>
2023-03-25 13:17:40 -0400

    feat(config): ui for reject in-hour updates

--- src/config/components/miscconfig.vue
+++ src/config/components/miscconfig.vue
@@ -4,6 +4,9 @@
     <b-form-checkbox id="province-tracking" v-model="mutableProvinceHighLowTracking">
       Track High/Low temps and precipitation data each day for the Manitoba province?
     </b-form-checkbox>
+    <b-form-checkbox id="reject-in-hour-condition-updates" v-model="mutableMisc.rejectInHourConditionUpdates">
+      Reject in-hour condition updates (only update the current conditions once an hour)
+    </b-form-checkbox>
     <b-button variant="success" class="mt-3" @click="saveMiscConfig" :disabled="saveState.saving">Save</b-button>
   </div>
 </template>
@@ -12,6 +15,7 @@
 export default {
   name: "config-province-tracking",
   props: {
+    misc: Object,
     provinceHighLowTracking: Boolean,
     saveState: {
       type: Object,
@@ -24,6 +28,7 @@ export default {
   data() {
     return {
       mutableProvinceHighLowTracking: false,
+      mutableMisc: {},
     };
   },
 
@@ -34,10 +39,11 @@ export default {
   methods: {
     createMutableMisc() {
       this.mutableProvinceHighLowTracking = this.provinceHighLowTracking;
+      this.mutableMisc = { ...this.misc };
     },
 
     saveMiscConfig() {
-      this.$emit("save", { provinceHighLowTracking: this.mutableProvinceHighLowTracking });
+      this.$emit("save", { provinceHighLowTracking: this.mutableProvinceHighLowTracking, misc: this.mutableMisc });
     },
   },
 };

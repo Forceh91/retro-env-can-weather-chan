7307c6be51530c74befdfd3818002e3138003e95
Matt Hadden <forceh2010@gmail.com>
2023-04-07 12:59:36 -0400

    feat(date-utils): day of year with leap year

--- date-utils.js
+++ date-utils.js
@@ -1,4 +1,4 @@
-const { parseISO, compareAsc, compareDesc, format } = require("date-fns");
+const { parseISO, compareAsc, compareDesc, format, getDayOfYear, getDaysInYear } = require("date-fns");
 
 function isWinterSeason(month) {
   // remember that months are 0 indexed
@@ -101,6 +101,18 @@ function convertECCDateStringToDateObject(eccDateString) {
   const cleanTextSummary = eccDateString.replace(" at", "").replace(",", "");
   return new Date(cleanTextSummary);
 }
+
+function getDayOfYearAdjustedForLeapDay() {
+  // returns the current day of the year (1-366) and if its a leap year will offset by 1 so that Dec 31st will always be day 366
+  const isLeapYear = getDaysInYear(Date.now()) === 366;
+  const dayOfYear = getDayOfYear(Date.now());
+  const month = new Date().getMonth() + 1;
+
+  // adjust by a day of its not a leap year and we're past feb
+  return dayOfYear + (!isLeapYear && month > 2 ? 1 : 0);
+}
+
+getDayOfYearAdjustedForLeapDay();
 
 module.exports = {
   isWinterSeason,

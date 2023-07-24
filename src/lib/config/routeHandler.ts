606051c4230cc808c379365a2c870c96cb2b6587
Matt Hadden <forceh2010@gmail.com>
2023-07-24 18:31:59 -0400

    feat(api): setup init route

--- src/lib/config/routeHandler.ts
+++ src/lib/config/routeHandler.ts
@@ -6,3 +6,11 @@ const config = initializeConfig();
 export function getConfigHandler(req: Request, res: Response) {
   res.json({ config: config.config, crawler: config.crawlerMessages });
 }
+
+export function getInitHandler(req: Request, res: Response) {
+  res.json({
+    config: { font: config.lookAndFeel.font, provinceHighLowEnabled: config.provinceHighLowEnabled },
+    crawler: config.crawlerMessages,
+    flavour: config.flavour,
+  });
+}
--- src/routes/init.ts
+++ src/routes/init.ts
@@ -1,10 +1,11 @@
 import express from "express";
+import { getInitHandler } from "lib/config";
 
 /*
  * "/" here represents "/init"
  */
 
 const router = express.Router();
-router.get("/", () => {});
+router.get("/", getInitHandler);
 
 export default router;

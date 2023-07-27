815cdb040445b51ecf06618967c47c4249d4a1e9
Matt Hadden <forceh2010@gmail.com>
2023-07-27 19:20:57 -0400

    refactor(forecast): use new formatter

--- src/display/components/screens/forecast.tsx
+++ src/display/components/screens/forecast.tsx
@@ -2,7 +2,8 @@ import { useEffect, useMemo, useState } from "react";
 import { WeatherStation } from "types";
 import { AutomaticScreenProps } from "types/screen.types";
 import { Conditions } from "../weather";
-import { DISPLAY_MAX_CHARACTERS_PER_LINE, SCREEN_DEFAULT_DISPLAY_LENGTH } from "consts";
+import { SCREEN_DEFAULT_DISPLAY_LENGTH } from "consts";
+import { formatStringTo8x32 } from "lib/display";
 
 type ForecastScreenProps = {
   weatherStationResponse: WeatherStation;
@@ -20,31 +21,6 @@ export function ForecastScreen(props: ForecastScreenProps) {
       else onComplete();
     }, SCREEN_DEFAULT_DISPLAY_LENGTH * 1000);
   }, [page]);
-
-  const formatForecast = (forecast: string, maxLines: number) => {
-    // if the entire forecast is less than DISPLAY_MAX_CHARACTERS_PER_LINE, we can skip this
-    if (forecast.length < DISPLAY_MAX_CHARACTERS_PER_LINE) return forecast;
-
-    // we should follow the maxLinesx32 format here
-    let formattedForecast = forecast;
-    for (let line = 1, startPoint = 0; line <= maxLines; line++) {
-      // before we do anything, we can just run to the end of the string if we'll past it
-      if (startPoint + DISPLAY_MAX_CHARACTERS_PER_LINE >= forecast.length) {
-        formattedForecast = `${formattedForecast.slice(0)}\n`;
-        break;
-      }
-
-      // get the last space on this line and replace it with a line break
-      const ixOfLastSpace = formattedForecast.lastIndexOf(" ", DISPLAY_MAX_CHARACTERS_PER_LINE * line);
-      formattedForecast =
-        formattedForecast.slice(0, ixOfLastSpace).trim() + "\n" + formattedForecast.slice(ixOfLastSpace).trim();
-
-      startPoint += DISPLAY_MAX_CHARACTERS_PER_LINE;
-    }
-
-    // truncate the string at the last line break and do some trimming
-    return formattedForecast.slice(0, formattedForecast.lastIndexOf("\n")).trim();
-  };
 
   const getForecastsForPage = () => {
     // todo:make this much nicer
@@ -69,7 +45,10 @@ export function ForecastScreen(props: ForecastScreenProps) {
     if (!weatherStationResponse?.forecast || page !== 1) return "";
 
     // by default we're allowed 4 lines for the forecast but if there's a warning/watch showing it'll be 3
-    return formatForecast(`Forecast for ${immediateForecast.period}..${immediateForecast.abbreviatedTextSummary}`, 4);
+    return formatStringTo8x32(
+      `Forecast for ${immediateForecast.period}..${immediateForecast.abbreviatedTextSummary}`,
+      4
+    );
   }, [weatherStationResponse?.stationTime?.observedDateTime]);
 
   // format the first forecast on the current page
@@ -77,7 +56,7 @@ export function ForecastScreen(props: ForecastScreenProps) {
     if (page === 1) return "";
 
     const forecast = forecastsForPage[0];
-    return formatForecast(`${forecast.period}..${forecast.abbreviatedTextSummary}`, 3);
+    return formatStringTo8x32(`${forecast.period}..${forecast.abbreviatedTextSummary}`, 3);
   }, [weatherStationResponse?.stationTime?.observedDateTime, page]);
 
   // format the second forecast on the current page
@@ -85,7 +64,7 @@ export function ForecastScreen(props: ForecastScreenProps) {
     if (page === 1) return "";
 
     const forecast = forecastsForPage[1];
-    return formatForecast(`${forecast.period}..${forecast.abbreviatedTextSummary}`, 3);
+    return formatStringTo8x32(`${forecast.period}..${forecast.abbreviatedTextSummary}`, 3);
   }, [weatherStationResponse?.stationTime?.observedDateTime, page]);
 
   // no response from weather station so whatever

7df25301288ee31c15330e0e651b5ba216b0e819
Matt Hadden <forceh2010@gmail.com>
2023-07-28 15:46:38 -0400

    fix: weather hook state update

--- src/hooks/weather.ts
+++ src/hooks/weather.ts
@@ -15,9 +15,14 @@ export function useWeatherEventStream() {
   // add an event listener for condition_update
   weatherEventStream &&
     weatherEventStream.addEventListener(CONDITIONS_EVENT_STREAM_CONDITION_UPDATE_EVENT, (conditionUpdate) => {
-      const parsedConditionUpdate = JSON.parse(conditionUpdate.data);
+      const parsedConditionUpdate = JSON.parse(conditionUpdate.data) as WeatherStation;
       if (!parsedConditionUpdate) return;
 
+      // if its the same observation date (down to the min/sec) then skip updating the state because it'll render too much
+      if (parsedConditionUpdate.stationTime?.observedDateTime === currentConditions?.stationTime?.observedDateTime)
+        return;
+
+      // update the state (and eventually cause a re-render)
       setCurrentConditions(parsedConditionUpdate);
     });
 

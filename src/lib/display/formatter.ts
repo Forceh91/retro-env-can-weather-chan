042907f9e0c7a3d350b990e0f08dd73049393c50
Matt Hadden <forceh2010@gmail.com>
2023-07-31 18:11:41 -0400

    fix(formatter): being weird if space may end line

--- src/lib/display/formatter.ts
+++ src/lib/display/formatter.ts
@@ -9,14 +9,15 @@ export function formatStringTo8x32(text: string, maxLines: number) {
   for (let line = 1, startPoint = 0; line <= maxLines; line++) {
     // before we do anything, we can just run to the end of the string if we'll past it
     if (startPoint + DISPLAY_MAX_CHARACTERS_PER_LINE >= text.length) {
-      formattedText = `${formattedText.slice(0)}\n`;
+      formattedText = `${formattedText.slice(0).trim()}\n`;
       break;
     }
 
-    // get the last space on this line and replace it with a line break
-    const ixOfLastSpace = formattedText.lastIndexOf(" ", DISPLAY_MAX_CHARACTERS_PER_LINE * line);
+    // get the last space on this line and replace it with a line break (put -1 here because if the last space is on the endpoint, it gets confused)
+    const ixOfLastSpace = formattedText.lastIndexOf(" ", DISPLAY_MAX_CHARACTERS_PER_LINE * line - 1);
     formattedText = formattedText.slice(0, ixOfLastSpace).trim() + "\n" + formattedText.slice(ixOfLastSpace).trim();
 
+    // move the cursor forward to the end of this line
     startPoint += DISPLAY_MAX_CHARACTERS_PER_LINE;
   }
 

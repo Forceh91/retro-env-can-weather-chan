f883b77e7fc86f3b3d8de53b1c344e7f60867350
Matt Hadden <forceh2010@gmail.com>
2023-08-03 19:16:44 -0400

    test: sunspot forecast truncation

--- src/__tests__/abbreviateConditions.test.ts
+++ src/__tests__/abbreviateConditions.test.ts
@@ -1,7 +1,7 @@
 import { harshTruncateConditions } from "lib/conditions";
 
 describe("condition truncation", () => {
-  test("harshTruncateConditions: makes sure condition string is only 13 characters", (done) => {
+  it("harshTruncateConditions: makes sure condition string is only 13 characters", (done) => {
     const conditionA = harshTruncateConditions("sunny");
     expect(conditionA).toBe("sunny");
 
@@ -14,7 +14,7 @@ describe("condition truncation", () => {
     done();
   });
 
-  test("harshTruncateConditions: handles light/heavy and rain/snowshower better", (done) => {
+  it("harshTruncateConditions: handles light/heavy and rain/snowshower better", (done) => {
     const conditionA = harshTruncateConditions("sunny");
     expect(conditionA).toBe("sunny");
 
@@ -37,7 +37,7 @@ describe("condition truncation", () => {
     done();
   });
 
-  test("harshTruncateConditions: handles light/heavy rain and snow", (done) => {
+  it("harshTruncateConditions: handles light/heavy rain and snow", (done) => {
     expect(harshTruncateConditions("light rain and snow")).toBe("rain/snow");
     expect(harshTruncateConditions("heavy rain and snow")).toBe("rain/snow");
     expect(harshTruncateConditions("light snow and rain")).toBe("rain/snow");
@@ -45,13 +45,13 @@ describe("condition truncation", () => {
     done();
   });
 
-  test("harshTruncateConditions: handles light/heavy freezing rain", (done) => {
+  it("harshTruncateConditions: handles light/heavy freezing rain", (done) => {
     expect(harshTruncateConditions("light freezing rain")).toBe("lgt frzg rain");
     expect(harshTruncateConditions("heavy freezing rain")).toBe("hvy frzg rain");
     done();
   });
 
-  test("harshTruncateConditions: handles light/heavy snow + blowing snow", (done) => {
+  it("harshTruncateConditions: handles light/heavy snow + blowing snow", (done) => {
     expect(harshTruncateConditions("light snow and blowing snow")).toBe("snow/blw snow");
     expect(harshTruncateConditions("heavy snow and blowing snow")).toBe("snow/blw snow");
     expect(harshTruncateConditions("light snow shower and blowing snow")).toBe("snow/blw snow");
@@ -59,25 +59,25 @@ describe("condition truncation", () => {
     done();
   });
 
-  test("harshTruncateConditions: handles light/heavy freezing drizzle", (done) => {
+  it("harshTruncateConditions: handles light/heavy freezing drizzle", (done) => {
     expect(harshTruncateConditions("light freezing drizzle")).toBe("lgt frzg drzl");
     expect(harshTruncateConditions("heavy freezing drizzle")).toBe("hvy frzg drzl");
     done();
   });
 
-  test("harshTruncateConditions: handles light/heavy rain + drizzle", (done) => {
+  it("harshTruncateConditions: handles light/heavy rain + drizzle", (done) => {
     expect(harshTruncateConditions("light rain and drizzle")).toBe("lgt rain/drzl");
     expect(harshTruncateConditions("heavy rain and drizzle")).toBe("hvy rain/drzl");
     done();
   });
 
-  test("harshTruncateConditions: handles light/heavy drizzle fog/mist", (done) => {
+  it("harshTruncateConditions: handles light/heavy drizzle fog/mist", (done) => {
     expect(harshTruncateConditions("light drizzle fog/mist")).toBe("light drizzle");
     expect(harshTruncateConditions("heavy drizzle fog/mist")).toBe("heavy drizzle");
     done();
   });
 
-  test("harshTruncateConditionss: gets rid of fog/mist if theres a space before it", (done) => {
+  it("harshTruncateConditionss: gets rid of fog/mist if theres a space before it", (done) => {
     expect(harshTruncateConditions("Light Snow Fog/Mist")).toBe("light snow");
     expect(harshTruncateConditions("Light Snow and Fog/Mist")).toBe("light snow");
     expect(harshTruncateConditions("Rain Fog/Mist")).toBe("rain");
@@ -87,13 +87,38 @@ describe("condition truncation", () => {
     done();
   });
 
-  test("harshTruncateConditionss: handles [condition] and drizzle", (done) => {
+  it("harshTruncateConditionss: handles [condition] and drizzle", (done) => {
     expect(harshTruncateConditions("Light Drizzle and Fog")).toBe("light drizzle");
     done();
   });
 
-  test("harshTruncateConditionss: handles freezing fog correctly", (done) => {
+  it("harshTruncateConditionss: handles freezing fog correctly", (done) => {
     expect(harshTruncateConditions("Freezing Fog")).toBe("freezing fog");
     done();
+  });
+});
+
+describe("sunspot forecast truncation", () => {
+  it("harshTruncateConditions: removes 'then' from the forecast", () => {
+    expect(harshTruncateConditions("Cloud then Rain", undefined, true)).not.toContain(" then ");
+  });
+
+  it("harshTruncateConditions: handles some long forecast conditions", () => {
+    expect(harshTruncateConditions("Isolated Rain Showers then Partly Sunny", undefined, true)).toStrictEqual(
+      "isld showers"
+    );
+    expect(harshTruncateConditions("Slight Chance Showers And Thunderstorms", undefined, true)).toStrictEqual(
+      "chnc showers"
+    );
+    expect(harshTruncateConditions("Chance Showers And Thunderstorms", undefined, true)).toStrictEqual("chnc showers");
+    expect(harshTruncateConditions("Slight Chance Rain Showers", undefined, true)).toStrictEqual("chnc showers");
+    expect(harshTruncateConditions("Slight Chance Rain", undefined, true)).toStrictEqual("chnc rain");
+    expect(harshTruncateConditions("Scattered Rain Showers", undefined, true)).toStrictEqual("sctd showers");
+    expect(harshTruncateConditions("Areas of Fog then Mostly Sunny", undefined, true)).toBe("areas of fog");
+    expect(harshTruncateConditions("Patchy Fog then some", undefined, true)).toBe("patchy fog");
+    expect(harshTruncateConditions("Patchy Fog", undefined, true)).toBe("patchy fog");
+    expect(harshTruncateConditions("Scattered Showers And Thunderstorms", undefined, true)).toBe("sctd showers");
+    expect(harshTruncateConditions("Mostly Cloudy", undefined, true)).toBe("mostly cldy");
+    expect(harshTruncateConditions("Slight Chance Light Rain", undefined, true)).toBe("chc lgt rain");
   });
 });

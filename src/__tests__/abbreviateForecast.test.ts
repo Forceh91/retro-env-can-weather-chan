f4b345d0d2aad125f0097be2457988bcc8939a08
Matt Hadden <forceh2010@gmail.com>
2023-08-03 19:05:08 -0400

    test: forecast truncation

---
+++ src/__tests__/abbreviateForecast.test.ts
@@ -0,0 +1,29 @@
+import { abbreviateForecast } from "lib/conditions";
+
+describe("forecast truncation", () => {
+  test("abbreviateForecast", () => {
+    const forecastLengthWanted = 5;
+
+    expect(abbreviateForecast("", forecastLengthWanted)).toStrictEqual("");
+    expect(abbreviateForecast("high plus 1", forecastLengthWanted)).toStrictEqual("high +1");
+    expect(abbreviateForecast("high plus 3", forecastLengthWanted)).toStrictEqual("high +3");
+    expect(abbreviateForecast("high plus 5", forecastLengthWanted)).toStrictEqual("high +5");
+    expect(abbreviateForecast("high plus 12", forecastLengthWanted)).toStrictEqual("high +12");
+    expect(abbreviateForecast("high zero", forecastLengthWanted)).toStrictEqual("high 0");
+    expect(abbreviateForecast("low minus 6", forecastLengthWanted)).toStrictEqual("low -6");
+    expect(abbreviateForecast("low minus 16", forecastLengthWanted)).toStrictEqual("low -16");
+    expect(abbreviateForecast("low minus 24", forecastLengthWanted)).toStrictEqual("low -24");
+    expect(abbreviateForecast("low zero", forecastLengthWanted)).toStrictEqual("low 0");
+    expect(abbreviateForecast("wind northeast 20 km/h", forecastLengthWanted)).toStrictEqual("wind NE 20");
+    expect(abbreviateForecast("40 percent chance of showers", forecastLengthWanted)).toStrictEqual("40% chnc of shwrs");
+    expect(abbreviateForecast("100 percent chance of flurries", forecastLengthWanted)).toStrictEqual(
+      "100% chnc of flurries"
+    );
+    expect(abbreviateForecast("5 percent chance of flurries", forecastLengthWanted)).toStrictEqual(
+      "5% chnc of flurries"
+    );
+    expect(abbreviateForecast("wind southwest 20 km/h gusting to 40 km/h", forecastLengthWanted)).toStrictEqual(
+      "wind SW 20g40"
+    );
+  });
+});

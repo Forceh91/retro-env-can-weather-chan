e68450b67a6905810c268755f7d4a86876811379
Matt Hadden <forceh2010@gmail.com>
2023-08-03 18:47:21 -0400

    chore: add jest testing

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testPathIgnorePatterns: ["v1/*"],
+  moduleDirectories: ["node_modules", "src"],
+};
---
+++ src/__tests__/season.test.ts
@@ -0,0 +1,19 @@
+import { isSunSpotSeason, isWindchillSeason } from "lib/date";
+
+describe("seasons", () => {
+  test("isWindchillSeason: calculates if its windchill season correctly or not", () => {
+    const windchillMonths = [11, 12, 1, 2, 3, 4];
+    const notWindchillMonths = [5, 6, 7, 8, 9, 10];
+
+    windchillMonths.forEach((month) => expect(isWindchillSeason(month)).toBe(true));
+    notWindchillMonths.forEach((month) => expect(isWindchillSeason(month)).toBe(false));
+  });
+
+  test("isSunSpotSeason: calculates if its sunspot season correctly or not", () => {
+    const sunspotMonths = [2, 3];
+    const notSunspotMonths = [1, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+    sunspotMonths.forEach((month) => expect(isSunSpotSeason(month)).toBe(true));
+    notSunspotMonths.forEach((month) => expect(isSunSpotSeason(month)).toBe(false));
+  });
+});

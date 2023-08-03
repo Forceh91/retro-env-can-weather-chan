71c59b24de9aefe5fa9ed5069a8ef36f30babaa2
Matt Hadden <forceh2010@gmail.com>
2023-08-02 22:19:01 -0400

    fix(formatter): not splitting correctly

--- src/lib/display/formatter.ts
+++ src/lib/display/formatter.ts
@@ -14,11 +14,11 @@ export function formatStringTo8x32(text: string, maxLines: number) {
     }
 
     // get the last space on this line and replace it with a line break (put -1 here because if the last space is on the endpoint, it gets confused)
-    const ixOfLastSpace = formattedText.lastIndexOf(" ", DISPLAY_MAX_CHARACTERS_PER_LINE * line - 1);
+    const ixOfLastSpace = formattedText.lastIndexOf(" ", startPoint + DISPLAY_MAX_CHARACTERS_PER_LINE);
     formattedText = formattedText.slice(0, ixOfLastSpace).trim() + "\n" + formattedText.slice(ixOfLastSpace).trim();
 
     // move the cursor forward to the end of this line
-    startPoint += DISPLAY_MAX_CHARACTERS_PER_LINE;
+    startPoint = ixOfLastSpace;
   }
 
   // truncate the string at the last line break and do some trimming

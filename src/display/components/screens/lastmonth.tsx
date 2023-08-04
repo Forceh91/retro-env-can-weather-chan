85eb62ba1d903458b0f5939fa64ce0f7f66e2f32
Matt Hadden <forceh2010@gmail.com>
2023-08-04 14:52:53 -0400

    fix(lastmonth): handle no data

--- src/display/components/screens/lastmonth.tsx
+++ src/display/components/screens/lastmonth.tsx
@@ -19,12 +19,13 @@ export function LastMonthScreen(props: LastMonthScreenProps) {
   if (!city?.length || !lastMonth) return <></>;
 
   const month = format(subMonths(new Date(), 1), "MMMM");
-  const formatNumber = (value: number) => value.toFixed(1).toString().padStart(5);
-  const formatTemp = (value: number) => ((value > 0 ? "+" : "") + value.toFixed(1).toString()).padStart(5);
+  const formatNumber = (value: number) => (value !== undefined ? value.toFixed(1).toString().padStart(5) : "N/A");
+  const formatTemp = (value: number) =>
+    value !== undefined ? ((value > 0 ? "+" : "") + value.toFixed(1).toString()).padStart(5) : "N/A";
   const formatDayWithSuffix = (value: number) => {
     const date = new Date();
     date.setDate(value);
-    if (!isValid(date)) return value;
+    if (!isValid(date)) return value ?? "N/A";
 
     return format(date, "do").padStart(4);
   };
@@ -37,31 +38,31 @@ export function LastMonthScreen(props: LastMonthScreenProps) {
       </div>
       <div>
         {"Average High".padEnd(15)}
-        {formatNumber(lastMonth.actual.averageHigh)}
+        {formatNumber(lastMonth.actual?.averageHigh)}
         {"".padStart(4)}
-        {formatNumber(lastMonth.normal.temperature.max)}
+        {formatNumber(lastMonth.normal?.temperature.max)}
       </div>
       <div>
         {"Average Low".padEnd(15)}
-        {formatNumber(lastMonth.actual.averageLow)}
+        {formatNumber(lastMonth.actual?.averageLow)}
         {"".padStart(4)}
-        {formatNumber(lastMonth.normal.temperature.min)}
+        {formatNumber(lastMonth.normal?.temperature.min)}
       </div>
       <div>
         {"Precip (MM)".padEnd(15)}
-        {formatNumber(lastMonth.actual.totalPrecip)}
+        {formatNumber(lastMonth.actual?.totalPrecip)}
         {"".padStart(4)}
-        {formatNumber(lastMonth.normal.precip.amount)}
+        {formatNumber(lastMonth.normal?.precip.amount)}
       </div>
       <br />
       {/* on the original channel this line mentioned where in records the precip amount came (1st, 3rd, 9th, etc.)*/}
       <div>
         {"Warmest Temp.".padEnd(14)}
-        {formatTemp(lastMonth.actual.warmestDay.value)} on the {formatDayWithSuffix(lastMonth.actual.warmestDay.day)}.
+        {formatTemp(lastMonth.actual?.warmestDay.value)} on the {formatDayWithSuffix(lastMonth.actual?.warmestDay.day)}.
       </div>
       <div>
         {"Coldest Temp.".padEnd(14)}
-        {formatTemp(lastMonth.actual.coldestDay.value)} on the {formatDayWithSuffix(lastMonth.actual.coldestDay.day)}.
+        {formatTemp(lastMonth.actual?.coldestDay.value)} on the {formatDayWithSuffix(lastMonth.actual?.coldestDay.day)}.
       </div>
     </div>
   );

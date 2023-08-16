fa0b6d8f0d1d8f10427084bdd94eb4470b56408b
Matt Hadden <forceh2010@gmail.com>
2023-08-15 22:21:14 -0400

    fix: memory leak in weather event stream hook

--- src/hooks/weather.ts
+++ src/hooks/weather.ts
@@ -8,23 +8,29 @@ export function useWeatherEventStream() {
   const [currentConditions, setCurrentConditions] = useState<WeatherStation>();
 
   useEffect(() => {
+    // dont do anything if we have an event
+    if (weatherEventStream) return;
+
     // setup the weather event stream
-    setWeatherEventStream(new EventSource("api/v1/weather/live"));
+    const eventStream = new EventSource("api/v1/weather/live");
+
+    // add an event listener for condition_update
+    eventStream &&
+      eventStream.addEventListener(CONDITIONS_EVENT_STREAM_CONDITION_UPDATE_EVENT, (conditionUpdate) => {
+        const parsedConditionUpdate = JSON.parse(conditionUpdate.data) as WeatherStation;
+        if (!parsedConditionUpdate) return;
+
+        // if its the same observation date (down to the min/sec) then skip updating the state because it'll render too much
+        if (parsedConditionUpdate.stationTime?.observedDateTime === currentConditions?.stationTime?.observedDateTime)
+          return;
+
+        // update the state (and eventually cause a re-render)
+        setCurrentConditions(parsedConditionUpdate);
+      });
+
+    // store this for later reference
+    setWeatherEventStream(eventStream);
   }, []);
-
-  // add an event listener for condition_update
-  weatherEventStream &&
-    weatherEventStream.addEventListener(CONDITIONS_EVENT_STREAM_CONDITION_UPDATE_EVENT, (conditionUpdate) => {
-      const parsedConditionUpdate = JSON.parse(conditionUpdate.data) as WeatherStation;
-      if (!parsedConditionUpdate) return;
-
-      // if its the same observation date (down to the min/sec) then skip updating the state because it'll render too much
-      if (parsedConditionUpdate.stationTime?.observedDateTime === currentConditions?.stationTime?.observedDateTime)
-        return;
-
-      // update the state (and eventually cause a re-render)
-      setCurrentConditions(parsedConditionUpdate);
-    });
 
   return { currentConditions };
 }

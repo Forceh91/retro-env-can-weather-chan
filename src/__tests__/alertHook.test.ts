f34da2bc07fea1a19699f1932d098fbe6757ea07
Matt Hadden <forceh2010@gmail.com>
2023-08-24 18:19:18 -0400

    test: alerts hook

---
+++ src/__tests__/alertHook.test.ts
@@ -0,0 +1,63 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { renderHook } from "@testing-library/react";
+import moxios from "moxios";
+import axios from "lib/axios";
+import { useAlerts } from "hooks/alerts";
+import alertResponse from "./testdata/hooks/alerts.json";
+
+const initializeHook = () => renderHook(() => useAlerts())?.result?.current;
+
+describe("Alerts hook", () => {
+  beforeEach(() => moxios.install(axios));
+  afterEach(() => moxios.uninstall(axios));
+
+  it("gives us the alert state", async () => {
+    const { alerts, hasFetched, mostImportantAlert } = initializeHook();
+    expect(alerts).toHaveLength(0);
+    expect(hasFetched).toBeFalsy();
+    expect(mostImportantAlert).toBeNull();
+  });
+
+  it("fetches at every interval", () => {
+    jest.useFakeTimers();
+    initializeHook();
+    jest.advanceTimersToNextTimer();
+    expect(moxios.requests.count()).toBeGreaterThan(1);
+  });
+
+  it("stores the init response in state", () => {
+    const { alerts, mostImportantAlert, hasFetched } = initializeHook();
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({ status: 200, response: alertResponse });
+      expect(alerts).toStrictEqual(alertResponse.alerts);
+      expect(mostImportantAlert).toStrictEqual(alertResponse.alerts[0]);
+      expect(hasFetched).toBeTruthy();
+    });
+  });
+
+  it("handles a 4xx error state", () => {
+    const { alerts, mostImportantAlert, hasFetched } = initializeHook();
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({ status: 404 });
+      expect(alerts).toHaveLength(0);
+      expect(hasFetched).toBeTruthy();
+      expect(mostImportantAlert).toBeNull();
+    });
+  });
+
+  it("handles a 5xx error state", () => {
+    const { alerts, mostImportantAlert, hasFetched } = initializeHook();
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({ status: 500 });
+      expect(alerts).toHaveLength(0);
+      expect(hasFetched).toBeTruthy();
+      expect(mostImportantAlert).toBeNull();
+    });
+  });
+});

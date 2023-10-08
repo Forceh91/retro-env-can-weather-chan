16791e1691b8eda358ecf4f92a71df99a97c8d79
Matt Hadden <forceh2010@gmail.com>
2023-10-07 20:52:43 -0400

    chore: update scripts for running server

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,6 @@ module.exports = {
   preset: "ts-jest/presets/js-with-ts-esm",
 
   testEnvironment: "node",
-  testPathIgnorePatterns: ["v1/*"],
   moduleDirectories: ["node_modules", "src"],
   modulePathIgnorePatterns: ["testdata"],
   // from https://stackoverflow.com/a/57916712/15076557

8a6c77b4e723c3d0f0bf7932c5656d08bed22c21
Matt Hadden <forceh2010@gmail.com>
2023-10-13 15:23:20 -0400

    fix: datetime specific tests

--- src/__tests__/ecccDate.test.ts
+++ src/__tests__/ecccDate.test.ts
@@ -1,8 +1,20 @@
 import { ecccDateStringToTSDate, getShorthandMonthNamesForSeason } from "lib/date/ecccDate";
 import * as seasonsLib from "lib/date/season";
+import { EDT_OFFSET_MINS_FROM_UTC, MockDateSetup, setupMockDate } from "lib/tests/timezone";
+
+let mockDate: MockDateSetup;
 
 describe("Helpers for ECCC dates", () => {
-  beforeEach(() => jest.useRealTimers());
+  beforeEach(() => {
+    mockDate = setupMockDate();
+    mockDate.set({ offset: EDT_OFFSET_MINS_FROM_UTC });
+  });
+
+  afterEach(() => {
+    mockDate.reset();
+    jest.useRealTimers();
+  });
+
   it("parses to a date object correctly", () => {
     const ecccDateString = "Sunday August 13, 2023 at 12:00 EDT";
     const expectedDate = new Date(2023, 7, 13, 12, 0, 0);

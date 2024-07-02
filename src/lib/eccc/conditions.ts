87f4e2ebe72f233facdebbc90be0296c5d923e53
Matt Hadden <forceh2010@gmail.com>
2024-07-02 14:22:48 -0400

    fix(eccc): handle wind direction+visibility being null

--- src/lib/eccc/conditions.ts
+++ src/lib/eccc/conditions.ts
@@ -226,10 +226,14 @@ class CurrentConditions {
       wind: {
         speed: { value: windSpeedValue, units: windSpeedUnits },
         gust: { value: windGustValue, units: windGustUnits },
-        direction: { value: windDirectionValue },
+        direction: windDirection,
       },
-      visibility: { value: visibilityValue, units: visibilityUnits },
+      visibility,
     } = conditions;
+
+    // handle wind direction and visibility potentially being null
+    const { value: windDirectionValue = null } = windDirection ?? {};
+    const { value: visibilityValue = null, units: visibilityUnits = null } = visibility ?? {};
 
     // store it to our conditions
     this._conditions = {

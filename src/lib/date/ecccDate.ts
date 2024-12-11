c0d8bae2f77bbc6c593d1b93be46ead6d096875d
Matt Hadden <forceh2010@gmail.com>
2024-12-11 15:33:13 -0500

    fix(date): account for atlantic/newfoundland timezones better

--- src/lib/date/ecccDate.ts
+++ src/lib/date/ecccDate.ts
@@ -2,8 +2,13 @@ import { format } from "date-fns";
 import { getIsWinterSeason } from "./season";
 
 export function ecccDateStringToTSDate(date: string) {
-  // JS doesn't see ATS/NDT (newfoundland time) as a valid date for some reason
-  const fixedTimezone = date.replace("AST", "GMT-0300").replace("NDT", "GMT-0230").replace("NST", "GMT-0330");
+  // JS doesn't see atlantic and newfoundland standard/daylight time as a valid time for some reason
+  const fixedTimezone = date
+    .replace("ADT", "GMT-0300")
+    .replace("AST", "GMT-0400")
+    .replace("NDT", "GMT-0230")
+    .replace("NST", "GMT-0330");
+
   return new Date(fixedTimezone.replace(" at", "").replace(",", ""));
 }
 
